f632e6e9eeb22621d95913fc1221c1fe5ec5db2f
Corey <coreyshanahan01@gmail.com>
2024-01-22 15:51:10 +0000

    added links to buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +17,24 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatDividerModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Chaos Chef';
+  gitHubLink = 'https://github.com/Corshan/chaos-chef';
+  demoVideoCode = '9zo-Qbt7ZFg';
+  iframeVideoLink = `https://www.youtube.com/embed/${this.demoVideoCode}`
+  buttonVideoLink = `https://www.youtube.com/watch/${this.demoVideoCode}`
+  posterLink = ''; 
+  urlSafe: SafeResourceUrl | undefined = undefined;
+
+  constructor(public sanitizer: DomSanitizer) {}
+
+
+  ngOnInit() {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.iframeVideoLink);
+  }
 }
